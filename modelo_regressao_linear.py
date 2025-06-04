2023f56861615410bf750020a4c68c4eb92ed972
robertopfneto <neto2014roberto@gmail.com>
2025-06-04 20:43:11 -0300

    Importando os dados do csv e convertendo pra tensores

---
+++ modelo_regressao_linear.py
@@ -0,0 +1,28 @@
+#%%
+import pandas as pd
+import torch
+import torch.nn as nn
+import seaborn as sns
+import numpy as np
+
+#%% importando dataset
+arquivo_carro = 'car_data.csv'
+carros = pd.read_csv(arquivo_carro)
+carros.head()
+# %% Visualizar modelo
+sns.scatterplot(x='Present_Price', y='Selling_Price', data=carros)
+sns.regplot(x='Present_Price', y='Selling_Price', data=carros)
+#%% convertendo para tensores
+
+# converte os dados em matrizes do numpy e redimensiono
+X_list = carros['Present_Price'].values
+X_np = np.array(X_list, dtype=np.float32).reshape(-1,1) #
+
+y_list = carros['Selling_Price'].values
+y_np = np.array(y_list, dtype=np.float32).reshape(-1,1)
+
+# transformo as matrizes resultantes em tensores do pytorch
+X = torch.from_numpy(X_np) 
+y_true = torch.from_numpy(y_np)
+
+# %% Classe do Modelo
