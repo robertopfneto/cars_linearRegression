1a889aae32f68e4f99367198a4402bc6cfa132cc
robertopfneto <neto2014roberto@gmail.com>
2025-06-05 16:59:28 -0300

    implementando classe do modelo e metodo de treinamento

--- modelo_regressao_linear.py
+++ modelo_regressao_linear.py
@@ -26,3 +26,62 @@ X = torch.from_numpy(X_np)
 y_true = torch.from_numpy(y_np)
 
 # %% Classe do Modelo
+class LinearRegressionTorch(nn.Module):
+    def __init__(self, input_size, output_size): 
+        super(LinearRegressionTorch, self).__init__()
+        self.linear = nn.Linear(input_size, output_size)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+input_dim = 1
+output_dim = 1
+model = LinearRegressionTorch(input_dim, output_dim)
+# %% Função de Perda
+loss_func = nn.MSELoss()
+
+#%% Optimizer
+LR = 0.02 #testar diferentes valores (01 ate 0.001)
+optimizer = torch.optim.SGD(model.parameters(), lr=LR)
+
+losses, slope, bias = [],[],[]
+
+#%% treinamento
+EPOCHS = 1000
+for epoch in range(EPOCHS):
+    # zerando gradiente
+    optimizer.zero_grad()
+
+    #forward pass
+    y_pred = model(X)
+
+    # computa perda
+    loss = loss_func(y_pred,y_true)
+    loss.backward()
+
+    # atualizando pesos
+    optimizer.step()
+
+    # pegando parametros
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            if name == 'linear.weight':
+                slope.append(param.data.numpy()[0][0])
+            if name == 'lienar.bias':
+                bias.append(param.data.numpy()[0])
+            
+    losses.append(float(loss.data))
+    if epoch % 100 == 0: # a cada 100 epocas
+        print('Epoch: {}, Loss: {:.4f}'.format(epoch, loss.data))
+
+#%% visualizar treinamento
+sns.scatterplot(x=range(EPOCHS), y=losses) #exibe grafico de perda
+
+#%% atualizacao do bias
+sns.scatterplot(x=range(EPOCHS), y=bias) #exibe grafico de perda
+
+# atualizacao da delividade
+sns.scatterplot(x=range(EPOCHS), y=slope) #exibe grafico de perda
+
+# chegando resultados
